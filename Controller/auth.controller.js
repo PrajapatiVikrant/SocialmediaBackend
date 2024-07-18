6a7d16a2127a32d818fb962e29b30f719e5bab6a
PrajapatiVikrant <prajapati865039@gmail.com>
2024-07-18 12:27:00 +0530

    add

--- Controller/auth.controller.js
+++ Controller/auth.controller.js
@@ -2,80 +2,80 @@ const ProfileSchema = require("../Model/ProfileSchema.js");
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
-const auth = {
-  signup: async (req, res) => {
-    console.log('game has started')
-    const {name,email,password,title} = req.query;
-     try{
-          const user = await ProfileSchema.findOne({email:email});
-          if(user){
-           return res.json({
-              message:'user already exist'
-            })
-          }
-          const hashPassword = await bcrypt.hash(password,10);
-          const data  = new ProfileSchema({
-            name:name,
-            email:email,
-            title:title,
-            password:hashPassword 
-          })
-          await data.save();
-          return res.json({
-            message:'Signup successfully',
-          })
-     }catch(error){
-      console.log('signup')
-      res.json({
-        message:'server error'
-      })
-     }
-  },
-  login: async (req, res) => {
-    const {email,password} = req.params;
-    try {
-      const data = await ProfileSchema.findOne({email:email});
-      if(!data){
-        return res.json({
-          message:'You are not exist register now'
-        })
-      }
-      const passwordmatch = await bcrypt.compare(password,data.password);
-      if(passwordmatch){
-       
-        const token =  jwt.sign(
-          { email: email,id:data._id},
-          process.env.JWT_SECRET,
-          { expiresIn: "60m" }
-          );
-         return res.json({
-          id:data._id,
-          message:'Login successfully',
-          token:token
-        })
-      }
-      return res.json({
-        message:'Invalid detail'
-      })
-      
-    } catch (error) {
-      console.log('login')
-      res.json({
-        message:'server error'
-      })
-    }
-  },
-  checklogin: (req,res)=>{
-   return res.json({
-    message:true
-   })
-  }
-};
 // const auth = {
-//   login:(req,res)=>{
-//     res.json({
-//       message:"hello world"
-//     })
+//   signup: async (req, res) => {
+//     console.log('game has started')
+//     const {name,email,password,title} = req.query;
+//      try{
+//           const user = await ProfileSchema.findOne({email:email});
+//           if(user){
+//            return res.json({
+//               message:'user already exist'
+//             })
+//           }
+//           const hashPassword = await bcrypt.hash(password,10);
+//           const data  = new ProfileSchema({
+//             name:name,
+//             email:email,
+//             title:title,
+//             password:hashPassword 
+//           })
+//           await data.save();
+//           return res.json({
+//             message:'Signup successfully',
+//           })
+//      }catch(error){
+//       console.log('signup')
+//       res.json({
+//         message:'server error'
+//       })
+//      }
+//   },
+//   login: async (req, res) => {
+//     const {email,password} = req.params;
+//     try {
+//       const data = await ProfileSchema.findOne({email:email});
+//       if(!data){
+//         return res.json({
+//           message:'You are not exist register now'
+//         })
+//       }
+//       const passwordmatch = await bcrypt.compare(password,data.password);
+//       if(passwordmatch){
+       
+//         const token =  jwt.sign(
+//           { email: email,id:data._id},
+//           process.env.JWT_SECRET,
+//           { expiresIn: "60m" }
+//           );
+//          return res.json({
+//           id:data._id,
+//           message:'Login successfully',
+//           token:token
+//         })
+//       }
+//       return res.json({
+//         message:'Invalid detail'
+//       })
+      
+//     } catch (error) {
+//       console.log('login')
+//       res.json({
+//         message:'server error'
+//       })
+//     }
+//   },
+//   checklogin: (req,res)=>{
+//    return res.json({
+//     message:true
+//    })
 //   }
-// }
+// };
+const auth = {
+  login:(req,res)=>{
+    res.json({
+      message:"hello world"
+    })
+  }
+}
  module.exports = auth;
