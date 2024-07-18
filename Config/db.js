f2dd61547c91c2cfd6f1353665cdcb99a0456d5a
PrajapatiVikrant <prajapati865039@gmail.com>
2024-07-18 16:54:09 +0530

    add

--- Config/db.js
+++ Config/db.js
@@ -1,18 +1,4 @@
 const mongoose = require("mongoose");
 const { config } = require("dotenv")
 config()
-async function databaseConnectiviy(){
-    try {
-        const { connection } = await mongoose.connect(process.env.MONGOOSE_URL);
-     if(connection){
-        console.log(connection.host)
-     }
-    } catch (error) {
-        console.log(error)
-        process.exit();
-    }
-    
- 
-}
-
-module.exports = databaseConnectiviy;
+mongoose.connect(process.env.MONGOOSE_URL);
