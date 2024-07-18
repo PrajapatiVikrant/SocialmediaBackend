37a55cca994add7007e8e0d90533c7d20fb73e3a
PrajapatiVikrant <prajapati865039@gmail.com>
2024-07-18 11:16:27 +0530

    Find error

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
 }))
-app.get('/socialmedia',auth)
+app.use('/socialmedia',auth)
 app.use('/socialmedia/auth',auth);
 app.use('/socialmedia/post',post)
 app.use('/socialmedia/profile',profile);
